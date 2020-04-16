c1bd30862f0d6c1ee29f7b62122c7dce155df060
Roc <597540267@qq.com>
2020-04-16 11:37:59 +0800

    Update uart.c

--- uart.c
+++ uart.c
@@ -7,15 +7,15 @@ static char fifo_buffer[128];
 void UART1_init(void) {
   fifo_init(&fifo, fifo_buffer, sizeof(fifo_buffer));
 
-	// baud = 115200
-	// 2MHz / 115200 = 17.36, 0x0011
-	UART1_BRR2 = 0x01;
-	UART1_BRR1 = 0x01;
-	
-	// Enable Receiver Interrupt  0x20
-	// Enable TX                  0x08
-	// Enable RX                  0x04
-	UART1_CR2 = 0x2c; 
+  // baud = 115200
+  // 2MHz / 115200 = 17.36, 0x0011
+  UART1_BRR2 = 0x01;
+  UART1_BRR1 = 0x01;
+
+  // Enable Receiver Interrupt  0x20
+  // Enable TX                  0x08
+  // Enable RX                  0x04
+  UART1_CR2 = 0x2c; 
 }
 
 void UART1_SendData8(unsigned char b) {
@@ -35,7 +35,7 @@ int UART1_PutBuffer(unsigned char b) {
   return kfifo_put(&fifo, b);
 }
 
-void UART1_ReceiveString(unsigned char *buf, unsigend char *len) {
+void UART1_ReceiveString(unsigned char *buf, unsigned char *len) {
   unsigned char i = 0;
   unsigned char b;
   while (kfifo_get(&fifo, &b) == 0) {
